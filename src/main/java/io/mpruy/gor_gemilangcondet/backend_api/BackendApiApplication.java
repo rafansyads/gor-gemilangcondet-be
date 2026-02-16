3d5e43928b6a141f6f5d15148e1d4ee8ed45c0f3
rafansyads <rafansya2005@gmail.com>
2026-02-16 16:05:09 +0700

    init project

---
+++ src/main/java/io/mpruy/gor_gemilangcondet/backend_api/BackendApiApplication.java
@@ -0,0 +1,13 @@
+package io.mpruy.gor_gemilangcondet.backend_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/mpruy/gor_gemilangcondet/backend_api/BackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mpruy.gor_gemilangcondet.backend_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
